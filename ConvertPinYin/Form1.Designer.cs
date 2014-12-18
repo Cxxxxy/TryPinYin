31de35795d8c4334895e588d35313d7f443123b3
Cxxxxy <reg6and7@gmail.com>
2014-12-18 13:25:37 +0800

    first edition

---
+++ ConvertPinYin/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace ConvertPinYin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReader = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnExport = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // btnReader
+            // 
+            this.btnReader.Location = new System.Drawing.Point(124, 437);
+            this.btnReader.Name = "btnReader";
+            this.btnReader.Size = new System.Drawing.Size(75, 23);
+            this.btnReader.TabIndex = 0;
+            this.btnReader.Text = "读取";
+            this.btnReader.UseVisualStyleBackColor = true;
+            this.btnReader.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Location = new System.Drawing.Point(12, 27);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(359, 404);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "汉字";
+            this.columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "拼音";
+            this.columnHeader2.Width = 150;
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(205, 437);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(75, 23);
+            this.btnExport.TabIndex = 3;
+            this.btnExport.Text = "导出";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(286, 437);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "退出";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "缩写";
+            this.columnHeader3.Width = 100;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 472);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnReader);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReader;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
+
---
+++ ConvertPinYin/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Office.Interop.Excel;
+
+
+namespace ConvertPinYin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+
+            FileStream fs = new FileStream ( "F:\\1.txt" , FileMode.Open , FileAccess.Read ) ;
+            StreamReader strR = new StreamReader(fs, Encoding.GetEncoding("gb2312")); //使用StreamReader类来读取文件 
+            strR.BaseStream.Seek ( 0 , SeekOrigin.Begin ) ; // 从数据流中读取每一行，直到文件的最后一行，并在richTextBox1中显示出内容             
+            string strLine = strR.ReadLine ( ) ;  
+
+            while ( strLine != null ) 
+            {
+                string strPinYin = null;
+                string strIniPinyin = null;
+                char[] arr = strLine.ToCharArray();
+                string[] strWordArr = new string[arr.Length];
+                for (int i = 0; i < arr.Length; i++) strWordArr[i] = arr[i].ToString();
+
+                foreach (string strSingle in strWordArr)
+                {                    
+                    strPinYin += Pinyin.Convert(strSingle);
+                    strIniPinyin += IniPinYin.getSpell(strSingle);
+                }
+
+                string[] InsertItem = { strLine, strPinYin, strIniPinyin };
+                ListViewItem lvi = new ListViewItem( InsertItem );
+                listView1.Items.Insert(listView1.Items.Count,lvi);
+                strLine = strR.ReadLine ( ) ; 
+            } 
+            strR.Close();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Microsoft.Office.Interop.Excel.Application ExcelApp = new Microsoft.Office.Interop.Excel.Application();
+                object m_objOpt = System.Reflection.Missing.Value;
+                Microsoft.Office.Interop.Excel.Workbooks ExcelBooks = (Microsoft.Office.Interop.Excel.Workbooks)ExcelApp.Workbooks;
+                Microsoft.Office.Interop.Excel._Workbook ExcelBook = (Microsoft.Office.Interop.Excel._Workbook)(ExcelBooks.Add(m_objOpt));
+                Microsoft.Office.Interop.Excel._Worksheet ExcelSheet = (Microsoft.Office.Interop.Excel._Worksheet)ExcelBook.ActiveSheet;
+
+                ExcelApp.Caption = "Test";                //设置标题
+                for (int i = 1; i <= listView1.Columns.Count; i++) ExcelSheet.Cells[2, i] = listView1.Columns[i - 1].Text;
+                for (int i = 3; i < listView1.Items.Count + 3; i++)
+                {
+                    ExcelSheet.Cells[i, 1] = listView1.Items[i - 3].Text;
+                    for (int j = 2; j <= listView1.Columns.Count; j++) ExcelSheet.Cells[i, j] = listView1.Items[i - 3].SubItems[j - 1].Text;
+                }
+                ExcelApp.Visible = true;
+            }
+            catch (SystemException sysE) { MessageBox.Show(sysE.ToString()); }          
+        }
+
+       
+    }
+}
